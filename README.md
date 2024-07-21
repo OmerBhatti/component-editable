9a13d2bfdf7e626d1949b6bcc43254d81170808a
Omer Bhatti <omer.sharif@arbisoft.com>
2024-07-21 17:34:45 +0500

    feat: added dynamic urls in styled HTMl for consistent media files and assets

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# component-editable
+# Component Editable
+With this `Editable` Wrapper you can make any React Component Editable and get `rawHtml` or `styledHtml` based on ypur need.
+
+### Usage
+ - Wrap any Component in Editable wrapper and pass `editable` true.
+ - You can get rawHtml or styledHtml whenever you need using `onChange` listener.
+```js
+import Editable from 'component-editable';
+
+function App() {
+  return (
+    <>
+      // ... some other components
+      <Editable
+        editable  // true if inner content is editable
+        onChange={(rawHtml, styledHtml) => console.log(rawHtml, styledHtml)}
+      >
+        <h1>Test Heading</h1>
+        <p className="read-the-docs">
+          This Content is placeholder
+        </p>
+      </Editable>
+      // ... some other components
+    </>
+  )
+}
+
+export default App;
+```
+
+### Props
+| prop | function | default |
+|------|----------|---------|
+|`editable`|Set if content is editable|true|
+|`onChange`|Listener to get rawHtml and styledHtml of Wrapeed Component|() => {}|
+|`rootSelector`|It's used to get the root of the react app for `styledHtml` |#root|
+|`linksRoot`|it's the starting path of assets used in page for `styledHtml` |/|
--- index.jsx
+++ index.jsx
@@ -3,13 +3,41 @@ import React from "react";
 const Editable = ({
 	children,
 	editable = true,
-	onChange = () => {} }
-) => {
+	rootSelector = "#root",
+	linksRoot = "/",
+	onChange = () => {}
+}) => {
+
+	const replaceLocalLinks = html_string => {
+		const html = new DOMParser().parseFromString(html_string, 'text/html');
+	
+		const domain = window.location.protocol + '//' + window.location.host;
+		const links = html.querySelectorAll(`a[href^="${linksRoot}"]`);
+		links.forEach(link => {
+		  link.href = link.href.replace(/^\.\//, domain + '/');
+		});
+	  
+		const images = html.querySelectorAll(`img[src^="${linksRoot}"]`);
+		images.forEach(image => {
+		  image.src = image.src.replace(/^\.\//, domain + '/');
+		});
+	
+		const headLinks = html.querySelectorAll(`link[href^="${linksRoot}"]`);
+		headLinks.forEach(link => {
+			link.href = link.href.replace(/^\.\//, domain + '/');
+		});
+	
+		const scripts = html.querySelectorAll(`script[src^="${linksRoot}"]`);
+		scripts.forEach(script => {
+			script.src = script.src.replace(/^\.\//, domain + '/');
+		});
+		return html.documentElement.outerHTML;
+	};
 
 	const convertRawHtmlToStyledHtml = rawHtml => {
 		const pageContent = document.querySelector("html").cloneNode(true);
-		pageContent.querySelector("#root").innerHTML = rawHtml;
-		const styledHtml = pageContent.innerHTML;
+		pageContent.querySelector(rootSelector).innerHTML = rawHtml;
+		const styledHtml = replaceLocalLinks(pageContent.innerHTML);
 		return styledHtml;
 	};
 
